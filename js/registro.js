0f625acd4e40f413d25aaa3cf4ff4e768691f19b
Zajith Corro <zaja_9508@hotmail.com>
2017-12-06 23:21:09 -0600

    Agregando ventana Registro: - HTML: * Juzgados * Lugares * Opciones de combo box
    - Javascript:
    * Hora automatica
    * Fecha automatica
    * Folio aleatorio

---
+++ js/registro.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    // Folio
+    let num = Math.floor(Math.random() * (1000 - 10) + 10);
+    document.getElementById('folioInput').value = num;
+
+    // Hora
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = '0' + date.getMinutes();
+    
+    let hora = hour + ':' + minute.slice(-2);
+    document.getElementById('horaInput').value = hora;
+
+    // Fecha
+    let day = '0' + date.getDate();
+    let month = '0' + (date.getMonth() + 1);
+    let year = date.getFullYear();
+    
+    let fecha = day.slice(-2) + '/' + month.slice(-2) + '/' + year;
+    document.getElementById('fechaInput').value = fecha;
+});
+
+function modal(){
+    $("#modalConfirmacion").modal();
+};
+
+function resetForm() {
+    document.getElementById('formRegistro').reset();
+}
+
+function getHome() {
+    location.href="http://127.0.0.1:5500/views/home.html"
+}
