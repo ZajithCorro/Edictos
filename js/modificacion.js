14a3a2573fff8aed2804c4ab2bf1cba6ca86f8e1
Zajith Corro <zaja_9508@hotmail.com>
2017-12-07 00:04:08 -0600

    Agregando ventana Modificacion: - HTML: * Juzgados * Lugares * Validaciones
    - Javascript:
    * Ocultar modal
    * Aparecer consultas

---
+++ js/modificacion.js
@@ -0,0 +1,8 @@
+function cerrar() {
+    console.log('Boton')
+    $('#modalOpciones').modal('hide');
+}
+
+function consultar() {
+    document.getElementById('resultados').classList.toggle('d-none')
+}
