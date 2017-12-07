3d76b861bcf31c4a39acf06fa1377c2e368ddce3
Zajith Corro <zaja_9508@hotmail.com>
2017-12-07 13:31:41 -0600

    Agregando pantalla Registro: - Javascript: * Activación de fechas dependiendo del número de publicaciones
    Agregando pantalla Forgot:
    - HTML:
    * Agregando link para redireccinar a página de inicio de sesión

--- js/registro.js
+++ js/registro.js
@@ -38,3 +38,127 @@ function getHome() {
     var url = path.slice(0, path.length - 13) + 'home.html'
     location.href = url;
 }
+
+function numPublicaciones(valor) {
+    let date1 = document.getElementById('fecha-1');
+    let date2 = document.getElementById('fecha-2');
+    let date3 = document.getElementById('fecha-3');
+    let date4 = document.getElementById('fecha-4');
+    let date5 = document.getElementById('fecha-5');
+    let date6 = document.getElementById('fecha-6');
+
+    if (valor === '1') {
+        date1.readOnly = false;
+        date1.required = true;
+
+        date2.readOnly = true;
+        date3.readOnly = true;
+        date4.readOnly = true;
+        date5.readOnly = true;
+        date6.readOnly = true;
+
+        date2.value = '';
+        date2.value = '';
+        date4.value = '';
+        date5.value = '';
+        date6.value = '';
+    }
+
+    if (valor === '2') {
+        date1.readOnly = false;
+        date2.readOnly = false;
+        date1.required = true;
+        date2.required = true;
+
+        date3.readOnly = true;
+        date4.readOnly = true;
+        date5.readOnly = true;
+        date6.readOnly = true;
+
+        date3.value = '';
+        date4.value = '';
+        date5.value = '';
+        date6.value = '';
+    }
+
+    if (valor === '3') {
+        date1.readOnly = false;
+        date2.readOnly = false;
+        date1.required = true;
+        date2.required = true;
+        date3.readOnly = false;
+        date3.required = true;
+
+        date4.readOnly = true;
+        date5.readOnly = true;
+        date6.readOnly = true;
+
+        date4.value = '';
+        date5.value = '';
+        date6.value = '';
+    }
+
+    if (valor === '4') {
+        date1.readOnly = false;
+        date2.readOnly = false;
+        date1.required = true;
+        date2.required = true;
+        date3.readOnly = false;
+        date3.required = true;
+        date4.readOnly = false;
+        date4.required = true;
+
+        date5.readOnly = true;
+        date6.readOnly = true;
+
+        date5.value = '';
+        date6.value = '';
+    }
+
+    if (valor === '5') {
+        date1.readOnly = false;
+        date2.readOnly = false;
+        date1.required = true;
+        date2.required = true;
+        date3.readOnly = false;
+        date3.required = true;
+        date4.readOnly = false;
+        date4.required = true;
+        date5.readOnly = false;
+        date5.required = true;
+
+        date6.readOnly = true;
+
+        date6.value = '';
+    }
+
+    if (valor === '6') {
+        date1.readOnly = false;
+        date2.readOnly = false;
+        date1.required = true;
+        date2.required = true;
+        date3.readOnly = false;
+        date3.required = true;
+        date4.readOnly = false;
+        date4.required = true;
+        date5.readOnly = false;
+        date5.required = true;
+        date6.readOnly = false;
+        date6.required = true;
+    }
+
+    if (valor === 'other') {
+        date1.readOnly = true;
+        date1.value = ''
+        date2.readOnly = true;
+        date2.value = ''
+        date3.readOnly = true;
+        date3.value = ''
+        date4.readOnly = true;
+        date4.value = ''
+        date5.readOnly = true;
+        date5.value = ''
+        date6.readOnly = true;
+        date6.value = ''
+    }
+}
