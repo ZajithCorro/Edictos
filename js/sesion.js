185eb133d0a46c8f3bc29156a71218bcafb236fa
Zajith Corro <zaja_9508@hotmail.com>
2017-12-07 02:16:05 -0600

    Agregando TODAS: - Javascript: * Link archivo global

--- js/sesion.js
+++ js/sesion.js
@@ -8,6 +8,10 @@ $('#formSesion').submit(function(e){
     let user = document.getElementById('numeroInput').value;
     let pass = document.getElementById('passInput').value;
 
+    // 1111 - Operador
+    // 2222 - Corrector
+    // 3333 - Hemeroteca
+
     if ((user === '1111' || user === '2222' || user === '3333' || user === '4444') && pass === 'pass') {
         localStorage.setItem('User', user);
 
