39f924c509f3aaae28f4f94a753c53cf83823811
Zajith Corro <zaja_9508@hotmail.com>
2017-12-06 22:32:13 -0600

    Agregando ventana Registro: - HTML: * Elementos requeridos * Elementos desactivados * Validaciones
    - Javascript:
    * Funciones de ventana emergente

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function modal(){
+    $("#modalConfirmacion").modal();
+};
+
+function resetForm() {
+    document.getElementById('formRegistro').reset();
+}
+
+function getHome() {
+    location.href="http://127.0.0.1:5500/views/home.html"
+}
